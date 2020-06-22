a8eb7ec5e1d5d92cfa73093bc6e7441670a8897a
huangxuanfeng <huangxuanfeng@gotokeep.com>
2020-06-22 19:19:57 +0800

    2

--- src/main/java/com/java/base/MyLog.java
+++ src/main/java/com/java/base/MyLog.java
@@ -4,4 +4,7 @@ package com.java.base;
  * Create by huangxuanfeng on 2020/5/19 下午7:18
  */
 public class MyLog {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
